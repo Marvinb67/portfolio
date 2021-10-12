2519d1b7793bf9e9bc98873ec6898d5a0658e689
Marvinb67 <brassler.marvin@gmail.com>
2021-10-12 11:12:54 +0200

    connexion

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'config/framework.php';
 require_once 'config/connect.php';
+
 ?>
 
 <!doctype html>
@@ -15,22 +16,20 @@ require_once 'config/connect.php';
   </head>
   <body>
     <form>
-    <h1>Connexion</h1>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+      <h1>Connexion</h1>
+      <p>Remplissez les champs suivant pour vous connecter.</p>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Mot de passe</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <button type="submit" class="btn btn-primary">Connexion</button>
+      <p>Vous n'avez pas de compte? <a href="register.php">Inscrivez vous</p>.
+    </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
