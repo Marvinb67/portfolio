a1ba8ec3b624ec551549338a37f2c99c705cc2be
Marvinb67 <brassler.marvin@gmail.com>
2021-10-12 15:24:37 +0200

    connexion

--- login.php
+++ login.php
@@ -1,6 +1,36 @@
 <?php
 require_once 'config/framework.php';
 require_once 'config/connect.php';
+$errors = [];
+if(isset($_POST['token']) && $_POST['token'] === $_SESSION['token']){
+
+  if(isset($_POST['email']) && !preg_match('#^[\w.-]+@[\w.-]+.[a-z]{2,6}$#i', $_POST['email'])) {
+    $errors['email'] = 'Adresse mail invalide';
+  }
+
+if(empty($errors)) {
+  $sql = "SELECT * FROM `users` WHERE 'email' = '".$_POST['email']."'";
+  if ($result = $mysqli->query($sql)) { 
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if(password_verify($_POST['mot_de_passe'], $user['password'])){
+            $_SESSION['users'] = $user;
+            redirectToRoute();
+            }else {
+              $errors['account'] = 'Compte invalide';
+            }
+
+        }
+        
+    }else {
+        $errors['account'] = 'Compte invalide';
+    }
+    
+    $result->close();
+    }
+  }
+}
+  
 
 ?>
 
@@ -15,19 +45,21 @@ require_once 'config/connect.php';
     <title>Connexion</title>
   </head>
   <body>
-    <form>
+    <form method = "POST">
+      <?= (isset($errors['account']) ? $errors['account'] : '');?>
+      <input type="hidden" name='token' value = "<?= minitoken()?>">
       <h1>Connexion</h1>
       <p>Remplissez les champs suivant pour vous connecter.</p>
       <div class="form-group">
         <label for="exampleInputEmail1">Email</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="email" name = "email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder = "jean-jacques@mail.fr">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Mot de passe</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" name = "mot_de_passe" class="form-control" id="exampleInputPassword1" placeholder = "Mot de passe">
       </div>
-      <button type="submit" class="btn btn-primary">Connexion</button>
+      <button type="submit" name = "connexion" class="btn btn-primary">Connexion</button>
       <p>Vous n'avez pas de compte? <a href="register.php">Inscrivez vous</p>.
     </form>
 
