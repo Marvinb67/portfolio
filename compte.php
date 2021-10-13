50137dff63f698014c35044134c5b272c3d4f611
Marvinb67 <brassler.marvin@gmail.com>
2021-10-13 11:48:00 +0200

    compte

--- compte.php
+++ compte.php
@@ -3,9 +3,9 @@ require_once 'config/framework.php';
 require_once 'config/connect.php';
 
 $errors = [];
-if(isset($_POST['inscription'])){
+if(isset($_POST['inscription_mail'])){
 
-  if(isset($_POST['token']) && $_POST['token'] === $_SESSION['token']){
+  if(isset($_POST['token_mail']) && $_POST['token_mail'] === $_SESSION['token_mail']){
 
   
 
@@ -14,46 +14,23 @@ if(isset($_POST['mail']) &&! preg_match('#^[\w.-]+@[\w.-]+.[a-z]{2,6}$#i', $_POS
   }else if(empty($_POST['mail'])){
     $errors['mail'] = 'Ce champ ne peut pas être vide';
   }
-  
-if(isset($_POST['password'])){  
-  if(empty($_POST['password'])){
-    $errors['password'] = 'Ce champ ne peut pas être vide';
-  }else {
-    $password_encoder = password_hash($_POST['password'],PASSWORD_DEFAULT);
-    //var_dump($password_encoder);//
-  }
-}
-
-if(isset($_POST['confirmation'])){
-  if(empty($_POST['confirmation'])){
-    $errors['confirmation'] = 'Ce champ ne peut pas être vide';
-  }else if($_POST['confirmation'] !== $_POST['password']){
-  $errors['confirmation'] = 'Les mots de passe ne correspondent pas';
-  }
-}
-
-if(isset($_POST['pseudo'])){
-  if(empty($_POST['pseudo'])){
-  $errors['pseudo'] = 'Ce champ ne peut pas être vide';
-}else if(strlen($_POST['pseudo']) <3 || strlen($_POST['pseudo']) >30){
-  $errors['pseudo'] = 'Votre pseudo doit faire entre 3 et 30 caractère';
-  }
-}
-
-  $roles = json_encode(['ROLE_USER']);
 
   if (empty($errors)){
-    $sql = "INSERT INTO `users`(`email`, `password`, `pseudo`, `roles`) VALUES ('".$_POST['mail']."','".$password_encoder."','".$_POST['pseudo']."','".$roles."')";
+    $sql = $sql = "UPDATE `users` SET (`email`='".$_POST['mail']."' WHERE id = '".$_SESSION['users']."')" ;
+
     if ($mysqli -> query($sql) === true) {
       redirectToRoute();
     }else {
       echo 'Erreur';
+     }
     }
-  }
   }
 }
 
-$sql = "UPDATE `users` SET (`email`='".$_POST['mail']."',`password`= '".$password_encoder."',`pseudo`='".$_POST['pseudo']."' WHERE id = '".$_SESSION['users']."')" ;
+ 
+
+
+
 
 ?>
 
@@ -72,33 +49,42 @@ $sql = "UPDATE `users` SET (`email`='".$_POST['mail']."',`password`= '".$passwor
   </head>
   <body>    
     <form method="POST">
-        <input type="hidden" name='token' value = "<?= minitoken()?>">
+        <input type="hidden" name='token_mail' value = "<?= minitoken()?>">
         <h1>Mon Compte</h1><br>
+        <h4>Modifier Email</h4><br>
         <div class="form-group">
             <label for="exampleInputEmail1">Modifier adresse email :</label>
             <input type="email" class="form-control <?= isset($errors['mail']) ? 'is-invalid' : '';?>" id="exampleInputEmail1" name= "mail" aria-describedby="emailHelp">
             <small id="emailHelp" class="form-text text-danger"><?= isset($errors['mail']) ? $errors['mail'] : '';?> </small>
+            <button type="submit" class="btn btn-warning" name="inscription_mail">Modifier</button>
         </div>
-    </form>
-        <form method ='POST'>
+     </form>
+     <form method="POST">
+        <input type="hidden" name='token_password' value = "<?= minitoken()?>">
+        <h4>Modifier Mot de passe</h4><br>
         <div class="form-group">
-            <label for="exampleInputPassword1">Modifier mot de passe :</label>
-            <input type="password" class="form-control <?= isset($errors['password']) ? 'is-invalid' : '';?>" id="exampleInputPassword1" name= "password">
-            <small id="passwordHelp" class="form-text text-danger"><?= isset($errors['password']) ? $errors['password'] : '';?> </small>
+            <label for="exampleInputPassword1"> Nouveau mot de passe :</label>
+            <input type="password" class="form-control <?= isset($errors['password']) ? 'is-invalid' : '';?>" id="exampleInputEmail1" name= "password" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-danger"><?= isset($errors['password']) ? $errors['password'] : '';?> </small>
+            <label for="exampleInputConfirmation1"> Confirmer le nouveau mot de passe :</label>
+            <input type="password" class="form-control <?= isset($errors['confirmation']) ? 'is-invalid' : '';?>" id="exampleInputEmail1" name= "confirmation" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-danger"><?= isset($errors['confirmation']) ? $errors['confirmation'] : '';?> </small>
+            <button type="submit" class="btn btn-warning" name="inscription">Modifier</button>
         </div>
-        </form>
+     </form>
+     <form method="POST">
+        <input type="hidden" name='token_pseudo' value = "<?= minitoken()?>">
+        <h4>Modifier pseudo</h4><br>
         <div class="form-group">
-            <label for="confirmationmp">Confirmation nouveau mot de passe :</label>
-            <input type="password" class="form-control <?= isset($errors['confirmation']) ? 'is-invalid' : '';?>" id="exampleInputPassword1"name = "confirmation">
-            <small id="confirmationHelp" class="form-text text-danger"><?= isset($errors['confirmation']) ? $errors['confirmation'] : '';?> </small>
+            <label for="exampleInputPseudo1">Modifier pseudo :</label>
+            <input type="text" class="form-control <?= isset($errors['pseudo']) ? 'is-invalid' : '';?>" id="exampleInputEmail1" name= "pseudo" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-danger"><?= isset($errors['pseudo']) ? $errors['pseudo'] : '';?> </small>
+            <button type="submit" class="btn btn-warning" name="inscription">Modifier</button>
         </div>
-        <div class="form-group">
-            <label for="pseudo">Modifier pseudo :</label>
-            <input type="text" class="form-control <?= isset($errors['pseudo']) ? 'is-invalid' : '';?>" id="pseudo"name = "pseudo">
-            <small id="pseudoHelp" class="form-text text-danger"><?= isset($errors['pseudo']) ? $errors['pseudo'] : '';?> </small>
-        </div>
-        <button type="submit" class="btn btn-primary" name="inscription">Envoyer</button>
-        <button onclick="return confirm('Are you sure you want to delete your account')" class = "btn btn-danger">Supprimer</button>
+     </form><br>
+
+        
+        <button onclick="return confirm('Are you sure you want to delete your account')" class = "btn btn-danger">Supprimer le compte</button>
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
