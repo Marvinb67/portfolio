ccd7aecc2e7acfe5ff84009700167e4b9a91f45e
Marvinb67 <brassler.marvin@gmail.com>
2021-10-14 16:05:21 +0200

    portfolio

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
-
+require_once 'vendor/autoload.php';
 require_once 'config/framework.php';
 require_once 'config/connect.php';
+require_once 'header.php';
+?>
+
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
